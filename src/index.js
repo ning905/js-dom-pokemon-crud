c35677e1f7e2f5c2095aa843bfdbe2f48f6fb1b7
Ning Ma <ningma905@gmail.com>
2022-07-04 17:55:17 +0100

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,75 @@
 const pokeForm = document.querySelector(".poke-form");
 const pokeList = document.querySelector(".poke-list");
+let state;
+
+function getState() {
+  return state;
+}
+
+function updateState(newState) {
+  state = [...newState];
+}
+
+function createDeleteBtn(pokemon) {
+  const btn = document.createElement("button");
+  btn.innerText = "Delete";
+  listenToDeleteBtn(btn, pokemon);
+
+  return btn;
+}
+
+function listenToDeleteBtn(btn, pokemon) {
+  btn.addEventListener("click", () => {
+    fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+      method: "DELETE",
+    });
+  });
+}
+
+function getBtnText(btn, pokemon) {
+  if (pokemon.like === true) {
+    btn.innerText = "Dislike";
+  } else {
+    btn.innerText = "Like";
+  }
+}
+
+function createLikeBtn(pokemon) {
+  const btn = document.createElement("button");
+  getBtnText(btn, pokemon);
+  listenToLikeBtn(btn, pokemon);
+  return btn;
+}
+
+function likePokemon(pokemon) {
+  fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ like: true }),
+  });
+}
+
+function dislikePokemon(pokemon) {
+  fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ like: false }),
+  });
+}
+
+function listenToLikeBtn(btn, pokemon) {
+  btn.addEventListener("click", () => {
+    if (pokemon.like === true) {
+      dislikePokemon(pokemon);
+    } else {
+      likePokemon(pokemon);
+    }
+  });
+}
 
 function addPokemon(pokemon) {
   const liEl = document.createElement("li");
@@ -11,12 +81,12 @@ function addPokemon(pokemon) {
 
   h2El.innerText = pokemon.name;
 
-  liEl.append(imgEl, h2El);
+  liEl.append(imgEl, h2El, createDeleteBtn(pokemon), createLikeBtn(pokemon));
   pokeList.append(liEl);
 }
 
 function addPokemons(pokemons) {
-  pokemons.forEach(pokemon => addPokemon(pokemon))
+  pokemons.forEach((pokemon) => addPokemon(pokemon));
 }
 
 function listenToAddPokemonForm() {
@@ -24,32 +94,34 @@ function listenToAddPokemonForm() {
     event.preventDefault();
     const pokemon = {
       name: pokeForm.name.value,
-      image: pokeForm.image.value
+      image: pokeForm.image.value,
     };
 
     // CREATE
-    // fetch("http://localhost:3000/pokemons", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify(pokemon)
-    // })
-    //   .then(res =>  res.json())
-    //   .then(pokemon => addPokemon(pokemon));
-    //   });
-
-    pokeForm.reset();
+    fetch("http://localhost:3000/pokemons", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pokemon),
+    })
+      .then((res) => res.json())
+      .then((pokemon) => addPokemon(pokemon));
   });
+
+  pokeForm.reset();
 }
 
 function init() {
   listenToAddPokemonForm();
 
   // READ
-  // fetch("http://localhost:3000/pokemons")
-  //   .then(res => res.json());
-  //   .then(pokemons => addPokemons(pokemons));
+  fetch("http://localhost:3000/pokemons")
+    .then((res) => res.json())
+    .then((pokemons) => {
+      updateState(pokemons);
+      addPokemons(state);
+    });
 }
 
 init();
